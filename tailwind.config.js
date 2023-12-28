910272f2f902efe1830c39c85bf24042c6599978
Mohammed Gouse <gpasha798@gmail.com>
2023-12-28 13:08:17 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'new-green': '#3c8067',
+        'gray':'#6c7289'
+      },
+    },
+  },
+  plugins: [],
+}
+
